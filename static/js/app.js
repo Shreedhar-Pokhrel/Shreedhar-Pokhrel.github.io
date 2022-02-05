53f7059e25d0be57e599a462e38b0b4466c95721
Shreedhar-Pokhrel <80855275+Shreedhar-Pokhrel@users.noreply.github.com>
2022-02-05 15:26:21 +0545

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,27 @@
+var columns = ['stock symbol','closing price','amount','121d avg','180d avg','diff7+','52w low','52w high','sector','values','headings']
+var table = d3.select('#base_table').append('table')
+var thead = table.append('thead')
+var tbody = table.append('tbody')
+
+	thead.append('tr')
+	  .selectAll('th')
+	    .data(columns)
+	    .enter()
+	  .append('th')
+	    .text((d) => d)
+
+var tabulate = function (data,columns) {
+  tbody.append('tr')
+	  .selectAll('td')
+	    .data(row => (columns.map(columns => data[columns])) )
+	    .enter()
+	  .append('td')
+	    .text((d) => d)
+
+}
+
+
+d3.csv('creation.csv',data=> {
+  tabulate(data,columns);
+  });
+
